0ef8180f796ee3783e19549639691c37412262a5
Harsh <harsh007dwivedi@gmail.com>
2019-06-21 15:43:06 +0530

    Payment Gateway using Stripe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "react-toastify/dist/ReactToastify.css";
+// import "./styles.css";
+
+toast.configure();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {
+        name: "Make Payment",
+        // price: 64998.67,
+        description: "Make Payment Easy"
+      }
+    };
+  }
+
+  async handleToken(token, product) {
+    const response = await axios.post(
+      "https://ry7v05l6on.sse.codesandbox.io/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="product">
+          <h1>{this.state.product.name}</h1>
+          {/* <h3>On Sale · ${this.state.product.price}</h3> */}
+        </div>
+        <StripeCheckout
+          stripeKey="pk_test_zgYbeVKDogsRsl7Ocxm3tjjZ00h6wiLonZ"
+          token={this.handleToken}
+          // amount={this.state.product.price * 100}
+          name="Payment Gateway"
+          billingAddress
+          shippingAddress
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
